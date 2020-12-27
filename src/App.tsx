a539c90e68ebab8a092b985906e72457de20efa5
naokihino178 <naoki.hino178@gmail.com>
2020-12-27 21:00:39 +0900

    問題文と回答を作成、再コミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { Button, Box } from "@material-ui/core";
 
-// function App() {
-//   const questions: string[] = ["apple", "grape", "banana"]; // 問題文の配列
-//   const questionsLength = questions.length; // 問題文の長さ
-//   // 最大値までのランダムな整数を返す関数
-//   const getRandomInt = (max: number) => {
-//     return Math.floor(Math.random() * Math.floor(max));
-//   };
+import { Button } from "@material-ui/core";
 
-interface questionsAndAnswer{[
-  questions: string,
-  answers: [string, string, string]
-]}
+const App = () => {
+  const [questionsAnswers, setQuestionsAnswers] = useState([
+    {
+      questions: "apple",
+      answers: ["リンゴ", "バナナ", "ブドウ"],
+    },
+    {
+      questions: "fox",
+      answers: ["キツネ", "タヌキ", "クマ"],
+    },
+  ]);
 
-function App() {
+  const [getQuestionsAnswers, setGetQuestionsAnswers] = useState({ // そもそもこれはstateで管理する必要があるのか？
+    questions: "tea",
+    answers: ["お茶", "牛乳", "コーヒー"],
+  });
+  
+  const questionsLength = questionsAnswers.length; // 問題文+回答のオブジェクトを要素とした配列の長さ（2）
+  const random = Math.random() * Math.floor(getQuestionsAnswers.answers.length);
 
-  const questions:questionsAndAnswer = [{
-    questions: "apple",
-    answers: ["リンゴ", "ゴリラ", "ラッパ"],
-  }
-]
-  const questionsLength = questions.length; // 問題文の長さ
   // 最大値までのランダムな整数を返す関数
   const getRandomInt = (max: number) => {
     return Math.floor(Math.random() * Math.floor(max));
   };
 
+  const changeQuestions = () => {
+    setGetQuestionsAnswers(questionsAnswers[getRandomInt(questionsLength)]);
+  };
+
   return (
     <div className="container">
-      <h3>{questions[getRandomInt(questionsLength)]}</h3>{" "}
+      <h3>{getQuestionsAnswers.questions}</h3>
       {/* 問題文からランダムに一つ値を取り出す */}
       <Button variant="contained" color="primary" className="buttonSize">
-        回答1（問題文と紐づけ）
+        {/* {getQuestionsAnswers.answers.shift()とか{getQuestionsAnswers.answers.splice()とかで、ランダムにとろうとしたけどダメだった、changeした後に何も表示されなくなる（配列が空になったのがなぜか引き疲れてる、問題文は表示されてるからレンダリングは問題なくできてるはずだけどなあ） */}
+        {getQuestionsAnswers.answers[0]}
       </Button>
       <Button variant="contained" color="primary" className="buttonSize">
-        回答2（問題文と紐づけ）
+        {getQuestionsAnswers.answers[1]}
       </Button>
       <Button variant="contained" color="primary" className="buttonSize">
-        回答3（問題文と紐づけ）
+        {getQuestionsAnswers.answers[2]}
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        size="large"
+        onClick={changeQuestions}
+      >
+        Change Questions
       </Button>
     </div>
   );
-}
+};
 
 export default App;
