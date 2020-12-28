d3807f8da82504ebd27ffaa9a3e3bcf0ceba2993
naokihino178 <naoki.hino178@gmail.com>
2020-12-28 21:51:59 +0900

    #3 問題をランダムで表示、正解不正解を判定、スコアの表示

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,49 @@ const App = () => {
       questions: "fox",
       answers: ["キツネ", "タヌキ", "クマ"],
     },
+    {
+      questions: "lion",
+      answers: ["ライオン", "タヌキ", "クマ"],
+    },
+    {
+      questions: "red",
+      answers: ["赤", "タヌキ", "クマ"],
+    },
+    {
+      questions: "car",
+      answers: ["車", "タヌキ", "クマ"],
+    },
+    {
+      questions: "buy",
+      answers: ["買う", "タヌキ", "クマ"],
+    },
+    {
+      questions: "have",
+      answers: ["持っている", "タヌキ", "クマ"],
+    },
+    {
+      questions: "snow",
+      answers: ["雪", "タヌキ", "クマ"],
+    },
+    {
+      questions: "fly",
+      answers: ["飛ぶ", "タヌキ", "クマ"],
+    },
+    {
+      questions: "run",
+      answers: ["走る", "タヌキ", "クマ"],
+    },
   ]);
 
-  const [getQuestionsAnswers, setGetQuestionsAnswers] = useState({ // そもそもこれはstateで管理する必要があるのか？
+  const [getQuestionsAnswers, setGetQuestionsAnswers] = useState({
+    // そもそもこれはstateで管理する必要があるのか？
     questions: "tea",
     answers: ["お茶", "牛乳", "コーヒー"],
   });
-  
+
+  const [score, setScore] = useState(0);
+  const [questionNumber, setQuestionNumber] = useState(1);
+
   const questionsLength = questionsAnswers.length; // 問題文+回答のオブジェクトを要素とした配列の長さ（2）
   const random = Math.random() * Math.floor(getQuestionsAnswers.answers.length);
 
@@ -29,31 +65,62 @@ const App = () => {
   };
 
   const changeQuestions = () => {
-    setGetQuestionsAnswers(questionsAnswers[getRandomInt(questionsLength)]);
+    //　配列をシャッフルする処理
+    for (let i = questionsAnswers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const tmp = questionsAnswers[i];
+      questionsAnswers[i] = questionsAnswers[j];
+      questionsAnswers[j] = tmp;
+    }
+    setGetQuestionsAnswers(questionsAnswers.shift()!); // !つけたらエラー消えた（nonnullアサーション）
   };
+  const correct = () => {
+    alert("正解！");
+    setScore(score + 1);
+    setQuestionNumber(questionNumber + 1);
+    changeQuestions();
+  };
+
+  const wrong = () => {
+    alert("残念！");
+    setQuestionNumber(questionNumber + 1);
+    changeQuestions();
+  };
+
+  if (questionNumber === 11) {
+    alert(`終了！ ${score * 10}点！`);
+  }
 
   return (
     <div className="container">
-      <h3>{getQuestionsAnswers.questions}</h3>
+      <h1>{getQuestionsAnswers.questions}</h1>
       {/* 問題文からランダムに一つ値を取り出す */}
-      <Button variant="contained" color="primary" className="buttonSize">
-        {/* {getQuestionsAnswers.answers.shift()とか{getQuestionsAnswers.answers.splice()とかで、ランダムにとろうとしたけどダメだった、changeした後に何も表示されなくなる（配列が空になったのがなぜか引き疲れてる、問題文は表示されてるからレンダリングは問題なくできてるはずだけどなあ） */}
+      <Button
+        variant="contained"
+        color="primary"
+        className="buttonSize"
+        onClick={correct}
+      >
+        {/* {getQuestionsAnswers.answers.shift()とか{getQuestionsAnswers.answers.splice()とかで、ランダムにとろうとしたけどダメだった、changeした後に何も表示されなくなる、spliceの戻り値がから配列だからかも、要確認、なぜshiftもダメ？（配列が空になったのがなぜか引き継がれてる、問題文は表示されてるからレンダリングは問題なくできてるはずだけどなあ） */}
         {getQuestionsAnswers.answers[0]}
-      </Button>
-      <Button variant="contained" color="primary" className="buttonSize">
-        {getQuestionsAnswers.answers[1]}
-      </Button>
-      <Button variant="contained" color="primary" className="buttonSize">
-        {getQuestionsAnswers.answers[2]}
       </Button>
       <Button
         variant="contained"
-        color="secondary"
-        size="large"
-        onClick={changeQuestions}
+        color="primary"
+        className="buttonSize"
+        onClick={wrong}
       >
-        Change Questions
+        {getQuestionsAnswers.answers[1]}
       </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        className="buttonSize"
+        onClick={wrong}
+      >
+        {getQuestionsAnswers.answers[2]}
+      </Button>
+      <h2>{`${questionNumber}問目　正解数: ${score}`}</h2>
     </div>
   );
 };
