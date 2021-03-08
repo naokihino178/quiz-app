0fafdc8f1848204df3b92ab30fcbd26ddfab7eef
naokihino178 <naoki.hino178@gmail.com>
2021-03-08 19:53:45 +0900

    #66 不正解の時は自動で次の問題へ行かないようにした

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlsdIODuFY3fZQF7oFIn1D6W4msZtLQ8M",
-  authDomain: "quizapp-9a366.firebaseapp.com",
-  projectId: "quizapp-9a366",
-  storageBucket: "quizapp-9a366.appspot.com",
-  messagingSenderId: "1028737921017",
-  appId: "1:1028737921017:web:b138dbd73d9129323e1728",
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // 初期化は一度だけ
